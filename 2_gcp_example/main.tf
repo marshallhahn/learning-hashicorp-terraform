28b927e67e53b913cae68c93d807c8ea968acecb
Marshall Hahn <git@marshallhahn.com>
2023-07-29 11:16:09 -0700

    gcp example - create vpc

---
+++ 2_gcp_example/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("potent-density-394317-9a30b5909c20.json")
+
+  project = "potent-density-394317"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
